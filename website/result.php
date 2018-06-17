482a4e699f312b6f738e1370b89a135e79cd518f
qweeer <luis.hoentsch@web.de>
2018-06-17 12:15:48 +0200

    utf-8

--- website/result.php
+++ website/result.php
@@ -24,6 +24,7 @@ if (isset($_GET['result'])) {
 <!DOCTYPE html>
 <html>
   <head>
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style type="text/css">
       html {overflow-x: hidden;}
